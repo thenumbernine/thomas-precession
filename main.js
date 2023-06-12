9c7df890a8d1cd0979414ab729c52ec101face00
Christopher Moore <christopher.e.moore@gmail.com>
2023-06-12 09:51:49 +0800

    switching to modules

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
+import {vec3, quat} from '/js/gl-matrix-3.4.1/index.js';
+import {DOM, getIDs, removeFromParent, show} from '/js/util.js';
+import {GLUtil} from '/js/gl-util.js';
+import {Mouse3D} from '/js/mouse3d.js';
+
 //TODO add a graph (at least to compare to theoretical values)
 //TODO add RK4 integrator
 
-var canvas;
-var gl;
-var glutil;
-var mouse;
-var sphereObj;
-var axisObj;
-var axisScale = 1.5;
+const ids = getIDs();
+window.ids = ids;
+
+const urlparams = new URLSearchParams(location.search);
+
+let canvas;
+let gl;
+let glutil;
+let mouse;
+let sphereObj;
+let axisObj;
+let sphereTex;
+
+let axisScale = 1.5;
 
 function resize() {
 	canvas.width = window.innerWidth;
@@ -61,29 +73,29 @@ dS/ds = (-Omega) * S
 */
 
 //story problem:
-var omega = .5;
-var radius = .5;
-var mass = .01;
+let omega = .5;
+let radius = .5;
+let mass = .01;
 //electron
-//var radius = 2.817940289458e-15;	//m
-//var omega = 5151228733459357.28;	// rad/s // angular velocity of the object's orbit
-//var mass = 9.10938291e0-31;	//kg
+//let radius = 2.817940289458e-15;	//m
+//let omega = 5151228733459357.28;	// rad/s // angular velocity of the object's orbit
+//let mass = 9.10938291e0-31;	//kg
 //earth
-//var omega = Math.PI * 2 / (60 * 60 * 24);	//rad/s
-//var radius = 6371000;	//earth radius in meters
-//var mass = 5.972e24;i	//kg
+//let omega = Math.PI * 2 / (60 * 60 * 24);	//rad/s
+//let radius = 6371000;	//earth radius in meters
+//let mass = 5.972e24;i	//kg
 
-var hbar = 1;		// angular velocity of the rotating object ... how is this different than omega?
-var gamma = 1 / Math.sqrt(1 - omega * omega * radius * radius);
+let hbar = 1;		// angular velocity of the rotating object ... how is this different than omega?
+let gamma = 1 / Math.sqrt(1 - omega * omega * radius * radius);
 
-var integrators = {
+let integrators = {
 	euler : function(t, x, dt, f) {
 		//return x + f(t,x) * t
 		//but javascript sucks and doesn't let you overload operators
 		return x.add(f(t,x).mul(dt));
 	}
 };
-var integrate = integrators.euler;
+let integrate = integrators.euler;
 
 function getX(t) {
 	return [
@@ -121,7 +133,7 @@ function State(args) {
 }
 State.prototype = {
 	add : function(b) {
-		var a = this;
+		let a = this;
 		return new State({
 			x : [
 				a.x[0] + b.x[0],
@@ -144,7 +156,7 @@ State.prototype = {
 		});
 	},
 	mul : function(b) {
-		var a = this;
+		let a = this;
 		return new State({
 			x : [
 				a.x[0] * b,
@@ -178,10 +190,10 @@ function getState(t) {
 	});
 }
 
-var dt = .01; 
-var t = 0;
-var state = getState(t);
-var startTime = undefined;
+let dt = .01; 
+let t = 0;
+let state = getState(t);
+let startTime = undefined;
 
 function update() {
 
@@ -195,9 +207,9 @@ function update() {
 	state = integrate(t, state, dt, function(t, state) {
 		
 		// a = du/dtau = -omega * omega * x;
-		var negOmegaSq = -omega * omega;
+		let negOmegaSq = -omega * omega;
 		
-		var a = [
+		let a = [
 			0,
 			state.x[1] * negOmegaSq,
 			state.x[2] * negOmegaSq,
@@ -205,12 +217,12 @@ function update() {
 		];
 
 		// dS/dtau = (u ^ a) * S
-		var dS_dt = [];
-		for (var i = 0; i < 4; ++i) {
+		let dS_dt = [];
+		for (let i = 0; i < 4; ++i) {
 			dS_dt[i] = 0;
-			for (var j = 0; j < 4; ++j) {
+			for (let j = 0; j < 4; ++j) {
 
-				var S_j = j==0 ? -state.S[j] : state.S[j];
+				let S_j = j==0 ? -state.S[j] : state.S[j];
 
 				//dS_dt = (u ^ a) * S
 				//dS_dt^i = (u ^ a)^ij * S_j
@@ -219,7 +231,7 @@ function update() {
 			}
 		}
 
-		var dState_dt = new State({
+		let dState_dt = new State({
 			x : state.u,
 			u : a,
 			S : dS_dt
@@ -236,182 +248,203 @@ function update() {
 	so J_uv is the tangent space to the rotation axis ... in the spatial space
 	*/
 	
-	var omegaAxis = vec3.fromValues(
+	let omegaAxis = vec3.fromValues(
 		state.S[1] * 5 / (2 * mass * radius * radius),
 		state.S[2] * 5 / (2 * mass * radius * radius),
 		state.S[3] * 5 / (2 * mass * radius * radius));
 	//and with the angular velocity, integrate by time (what kind of time?) to get the angle itself
-	var omegaAngle = vec3.length(omegaAxis);
+	let omegaAngle = vec3.length(omegaAxis);
 	if (Math.abs(omegaAngle) > 1e-20) {
-		var omegaQ = quat.create();
+		let omegaQ = quat.create();
 		vec3.scale(omegaAxis, omegaAxis, 1/omegaAngle);
 		omegaAngle *= dt;
 		quat.setAxisAngle(omegaQ, omegaAxis, omegaAngle);
 		quat.multiply(sphereObj.angle, sphereObj.angle, omegaQ);
 		quat.copy(axisObj.angle, sphereObj.angle);
 
-		axisObj.attrs.vertex.data[0] = axisScale*omegaAxis[0];
-		axisObj.attrs.vertex.data[1] = axisScale*omegaAxis[1];
-		axisObj.attrs.vertex.data[2] = axisScale*omegaAxis[2];
-		axisObj.attrs.vertex.data[3] = -axisScale*omegaAxis[0];
-		axisObj.attrs.vertex.data[4] = -axisScale*omegaAxis[1];
-		axisObj.attrs.vertex.data[5] = -axisScale*omegaAxis[2];
-		axisObj.attrs.vertex.updateData();
+		axisObj.attrs.vertex.buffer.data[0] = axisScale*omegaAxis[0];
+		axisObj.attrs.vertex.buffer.data[1] = axisScale*omegaAxis[1];
+		axisObj.attrs.vertex.buffer.data[2] = axisScale*omegaAxis[2];
+		axisObj.attrs.vertex.buffer.data[3] = -axisScale*omegaAxis[0];
+		axisObj.attrs.vertex.buffer.data[4] = -axisScale*omegaAxis[1];
+		axisObj.attrs.vertex.buffer.data[5] = -axisScale*omegaAxis[2];
+		axisObj.attrs.vertex.buffer.updateData();
 	}
 //console.log(state.S[0], state.S[1], state.S[2], state.S[3]);	
 	glutil.draw();
-	requestAnimFrame(update);
+	requestAnimationFrame(update);
 }
 
-$(document).ready(init1);
+canvas = DOM('canvas', {
+	css : {
+		left : 0,
+		top : 0,
+		position : 'absolute',
+		userSelect : 'none',
+	},
+	prependTo : document.body,
+});
 
-function init1() {
-	canvas = $('<canvas>', {
-		css : {
-			left : 0,
-			top : 0,
-			position : 'absolute'
-		}
-	}).prependTo(document.body).get(0);
-	$(canvas).disableSelection();
-
-	try {
-		glutil = new GLUtil({canvas:canvas});
-		gl = glutil.context;
-	} catch (e) {
-		panel.remove();
-		$(canvas).remove();
-		$('#webglfail').show();
-		throw e;
-	}
-	gl.enable(gl.DEPTH_TEST);
-	glutil.view.pos[0] = -6;
-	glutil.view.fovY = 45;
-	
-	//x fwd, z up, y left:
-	var SQRT_1_2 = Math.sqrt(1/2);
-	quat.mul(glutil.view.angle, /*90' x*/[SQRT_1_2,0,0,SQRT_1_2], /*90' -y*/[0,-SQRT_1_2,0,SQRT_1_2]);
-
-	sphereTex = new glutil.Texture2D({
-		flipY : true,
-		generateMipmap : true,
-		magFilter : gl.LINEAR,
-		minFilter : gl.LINEAR_MIPMAP_LINEAR,
-		url : 'BlueMarble.png',
-		onload : function() {
-			init2(this);
-		}
-	});
+try {
+	glutil = new GLUtil({canvas:canvas});
+	gl = glutil.context;
+} catch (e) {
+	panel.remove();
+	removeFromParent(canvas);
+	show(ids.webglfail);
+	throw e;
 }
+gl.enable(gl.DEPTH_TEST);
+glutil.view.pos[0] = -6;
+glutil.view.fovY = 45;
 
-function init2(sphereTex) {
-	var sphereShader = new glutil.ShaderProgram({
-		vertexCodeID : 'sphere-vsh',
-		fragmentCodeID : 'sphere-fsh'
-	});
+//x fwd, z up, y left:
+let SQRT_1_2 = Math.sqrt(1/2);
+quat.mul(glutil.view.angle, /*90' x*/[SQRT_1_2,0,0,SQRT_1_2], /*90' -y*/[0,-SQRT_1_2,0,SQRT_1_2]);
 
-	var w = 20;
-	var h = 10;
-	var sphereVtxArray = new Float32Array(3 * 2 * 3 * w * h);
-	var sphereTCArray = new Float32Array(3 * 2 * 3 * w * h);
-	var radius = 1;
-	var writePt = function(ofs, u, v) {
-		var phi = 2 * Math.PI * u;
-		var theta = Math.PI * v;
-		//should be x y z but im too lazy to correct the camera for y being up
-		var z = radius * Math.cos(phi) * Math.sin(theta);
-		var x = radius * Math.sin(phi) * Math.sin(theta);
-		var y = radius * Math.cos(theta);
-		sphereVtxArray[0 + ofs] = x;
-		sphereVtxArray[1 + ofs] = y;
-		sphereVtxArray[2 + ofs] = z;
-		sphereTCArray[0 + ofs] = u;
-		sphereTCArray[1 + ofs] = 1 - v;
-		sphereTCArray[2 + ofs] = 0;
-	};
-	for (var v = 0; v < h; v++) {
-		for (var u = 0; u < w; u++) {
-			writePt(0 + 18 * (u + w * v), u/w, v/h);
-			writePt(3 + 18 * (u + w * v), (u+1)/w, v/h);
-			writePt(6 + 18 * (u + w * v), (u+1)/w, (v+1)/h);
-			writePt(9 + 18 * (u + w * v), (u+1)/w, (v+1)/h);
-			writePt(12 + 18 * (u + w * v), u/w, (v+1)/h);
-			writePt(15 + 18 * (u + w * v), u/w, v/h);
+sphereTex = new glutil.Texture2D({
+	flipY : true,
+	generateMipmap : true,
+	magFilter : gl.LINEAR,
+	minFilter : gl.LINEAR_MIPMAP_LINEAR,
+	url : 'BlueMarble.png',
+	onload : () => {
+		let sphereShader = new glutil.Program({
+			vertexCode : `
+in vec3 vertex;
+in vec3 tc;
+out vec2 vtc;
+uniform mat4 mvMat;
+uniform mat4 projMat;
+void main() {
+	vec4 eyePos = mvMat * vec4(vertex, 1.);
+	vtc = tc.xy;
+	gl_Position = projMat * eyePos;
+}
+`,
+			fragmentCode : `
+in vec2 vtc;
+uniform sampler2D tex;
+out vec4 fragColor;
+void main() {
+	fragColor = texture(tex, vtc);
+}
+`,
+		});
+
+		let w = 20;
+		let h = 10;
+		let sphereVtxArray = new Float32Array(3 * 2 * 3 * w * h);
+		let sphereTCArray = new Float32Array(3 * 2 * 3 * w * h);
+		let radius = 1;
+		let writePt = function(ofs, u, v) {
+			let phi = 2 * Math.PI * u;
+			let theta = Math.PI * v;
+			//should be x y z but im too lazy to correct the camera for y being up
+			let z = radius * Math.cos(phi) * Math.sin(theta);
+			let x = radius * Math.sin(phi) * Math.sin(theta);
+			let y = radius * Math.cos(theta);
+			sphereVtxArray[0 + ofs] = x;
+			sphereVtxArray[1 + ofs] = y;
+			sphereVtxArray[2 + ofs] = z;
+			sphereTCArray[0 + ofs] = u;
+			sphereTCArray[1 + ofs] = 1 - v;
+			sphereTCArray[2 + ofs] = 0;
+		};
+		for (let v = 0; v < h; v++) {
+			for (let u = 0; u < w; u++) {
+				writePt(0 + 18 * (u + w * v), u/w, v/h);
+				writePt(3 + 18 * (u + w * v), (u+1)/w, v/h);
+				writePt(6 + 18 * (u + w * v), (u+1)/w, (v+1)/h);
+				writePt(9 + 18 * (u + w * v), (u+1)/w, (v+1)/h);
+				writePt(12 + 18 * (u + w * v), u/w, (v+1)/h);
+				writePt(15 + 18 * (u + w * v), u/w, v/h);
+			}
 		}
-	}
-	
-	sphereObj = new glutil.SceneObject({
-		mode : gl.TRIANGLES,
-		shader : sphereShader,
-		static : false,
-		uniforms : { 
-			tex : 0
-		},
-		attrs : { 
-			vertex : new glutil.ArrayBuffer({
-				data : sphereVtxArray
+		
+		sphereObj = new glutil.SceneObject({
+			mode : gl.TRIANGLES,
+			shader : sphereShader,
+			static : false,
+			uniforms : { 
+				tex : 0
+			},
+			attrs : { 
+				vertex : new glutil.ArrayBuffer({
+					data : sphereVtxArray
+				}),
+				tc : new glutil.ArrayBuffer({
+					data : sphereTCArray
+				})
+			},
+			texs : [sphereTex]
+		});
+
+		axisObj = new glutil.SceneObject({
+			mode : gl.LINES,
+			shader : new glutil.Program({
+				vertexCode : `
+in vec3 vertex;
+uniform mat4 mvMat;
+uniform mat4 projMat;
+void main() {
+	gl_Position = projMat * mvMat * vec4(vertex, 1.);
+}
+`,
+				fragmentCode : `
+out vec4 fragColor;
+void main() {
+	fragColor = vec4(0., 1., 1., 1.); 
+}
+`,
 			}),
-			tc : new glutil.ArrayBuffer({
-				data : sphereTCArray
-			})
-		},
-		texs : [sphereTex]
-	});
+			attrs : {
+				vertex : new glutil.ArrayBuffer({
+					data : [0,0,axisScale*radius, 0,0,-axisScale*radius],
+					usage : gl.DYNAMIC_DRAW
+				})
+			},
+			static : false
+		});
 
-	axisObj = new glutil.SceneObject({
-		mode : gl.LINES,
-		shader : new glutil.ShaderProgram({
-			vertexCodeID : 'axis-vsh',
-			fragmentCodeID : 'axis-fsh'
-		}),
-		attrs : {
-			vertex : new glutil.ArrayBuffer({
-				data : [0,0,axisScale*radius, 0,0,-axisScale*radius],
-				usage : gl.DYNAMIC_DRAW
-			})
-		},
-		static : false
-	});
+		let lastMouseRot = quat.create();	
+		let tmpQ = quat.create();
+		let tmpV = vec3.create();
+		mouse = new Mouse3D({
+			pressObj : canvas,
+			move : function(dx,dy) {
+				let rotAngle = Math.PI / 180 * .01 * Math.sqrt(dx*dx + dy*dy);
+				quat.setAxisAngle(tmpQ, [-dy, -dx, 0], rotAngle);
+				//mat4.translate(glutil.scene.mvMat, glutil.scene.mvMat, [10*dx/canvas.width, -10*dy/canvas.height, 0]);
 
-	var lastMouseRot = quat.create();	
-	var tmpQ = quat.create();
-	var tmpV = vec3.create();
-	mouse = new Mouse3D({
-		pressObj : canvas,
-		move : function(dx,dy) {
-			var rotAngle = Math.PI / 180 * .01 * Math.sqrt(dx*dx + dy*dy);
-			quat.setAxisAngle(tmpQ, [-dy, -dx, 0], rotAngle);
-			//mat4.translate(glutil.scene.mvMat, glutil.scene.mvMat, [10*dx/canvas.width, -10*dy/canvas.height, 0]);
+				//put tmpQ into the frame of glutil.view.angle, so we can rotate the view vector by it
+				//  lastMouseRot = glutil.view.angle-1 * tmpQ * glutil.view.angle
+				// now newViewAngle = glutil.view.angle * tmpQ = lastMouseRot * glutil.view.angle
+				// therefore lastMouseRot is the global transform equivalent of the local transform of tmpQ
+				quat.mul(lastMouseRot, glutil.view.angle, tmpQ);
+				quat.conjugate(tmpQ, glutil.view.angle);
+				quat.mul(lastMouseRot, lastMouseRot, tmpQ);
 
-			//put tmpQ into the frame of glutil.view.angle, so we can rotate the view vector by it
-			//  lastMouseRot = glutil.view.angle-1 * tmpQ * glutil.view.angle
-			// now newViewAngle = glutil.view.angle * tmpQ = lastMouseRot * glutil.view.angle
-			// therefore lastMouseRot is the global transform equivalent of the local transform of tmpQ
-			quat.mul(lastMouseRot, glutil.view.angle, tmpQ);
-			quat.conjugate(tmpQ, glutil.view.angle);
-			quat.mul(lastMouseRot, lastMouseRot, tmpQ);
+				vec3.copy(tmpV, glutil.view.pos);
+				let posDist = vec3.length(tmpV);
+				vec3.transformQuat(tmpV, tmpV, lastMouseRot);
+				vec3.normalize(tmpV, tmpV);
+				vec3.scale(tmpV, tmpV, posDist);
+				vec3.copy(glutil.view.pos, tmpV);
+				
+				//RHS apply so it is relative to current view 
+				//newViewAngle := glutil.view.angle * tmpQ
+				quat.mul(glutil.view.angle, lastMouseRot, glutil.view.angle);
+				quat.normalize(glutil.view.angle, glutil.view.angle);
 
-			vec3.copy(tmpV, glutil.view.pos);
-			var posDist = vec3.length(tmpV);
-			vec3.transformQuat(tmpV, tmpV, lastMouseRot);
-			vec3.normalize(tmpV, tmpV);
-			vec3.scale(tmpV, tmpV, posDist);
-			vec3.copy(glutil.view.pos, tmpV);
-			
-			//RHS apply so it is relative to current view 
-			//newViewAngle := glutil.view.angle * tmpQ
-			quat.mul(glutil.view.angle, lastMouseRot, glutil.view.angle);
-			quat.normalize(glutil.view.angle, glutil.view.angle);
-
-		},
-		zoom : function(dz) {
-			glutil.view.fovY *= Math.exp(-.0003 * dz);
-			glutil.view.fovY = Math.clamp(glutil.view.fovY, 1, 179);
-			glutil.updateProjection();
-		}
-	});
-	resize();
-	update();
-}
-
-
+			},
+			zoom : function(dz) {
+				glutil.view.fovY *= Math.exp(-.0003 * dz);
+				glutil.view.fovY = Math.clamp(glutil.view.fovY, 1, 179);
+				glutil.updateProjection();
+			}
+		});
+		resize();
+		update();
+	},
+});
